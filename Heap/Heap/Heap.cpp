88a642a84630b43541a50a215b22342fb4840196
4444753951 <115874579+4444753951@users.noreply.github.com>
2023-06-29 16:04:05 +0800

    堆

---
+++ Heap/Heap/Heap.cpp
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Heap.h"
+
+void HeapPrint(HP* php)
+{
+	assert(php);
+	for (int i = 0; i < php->size; i++)
+	{
+		printf("%d ", php->a[i]);
+	}
+	printf("\n");
+}
+void HeapInit(HP* php)
+{
+	assert(php);
+	php->a = NULL;
+	php->capacity = php->size = 0;
+}
+
+void HeapDestroy(HP* php)
+{
+	assert(php);
+	free(php->a);
+	php->a = NULL;
+	php->capacity = php->size = 0;
+}
+
+void Swap(HPDataType* p1, HPDataType* p2)
+{
+	HPDataType tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void AdjustUp(HPDataType* a, int child)
+{
+	int parent = (child - 1) / 2;
+	while (child>0 )
+	{
+		if (a[child] < a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void AdjustDown(HPDataType* a, int size, int parent)
+{
+	int child = parent * 2 + 1;
+	while (child < size)
+	{
+		// 选出左右孩子中小/大的那个
+		if (child + 1 < size
+			&& a[child + 1] < a[child])
+		{
+			++child;
+		}
+
+		if (a[child] < a[parent])
+		{
+			Swap(&a[parent], &a[child]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapPush(HP* php, HPDataType x)
+{
+	assert(php);
+	if (php->size == php->capacity)
+	{
+		int newcapacity = php->capacity == 0 ? 4: php->capacity * 2;
+		HPDataType* tmp = (HPDataType*)realloc(php->a, sizeof(HPDataType)*newcapacity);
+		if (NULL == tmp)
+		{
+			exit(-1);
+		}
+		php->a = tmp;
+		php->capacity = newcapacity;
+	}
+	php->a[php->size] = x;
+	php->size++;
+	AdjustUp(php->a, php->size - 1);
+}
+void HeapPop(HP* php)
+{
+	assert(php);
+	assert(php->size > 0);
+
+	Swap(&php->a[0], &php->a[php->size - 1]);
+	php->size--;
+	AdjustDown(php->a, php->size,0);
+}
+HPDataType HeapTop(HP* php);
+bool HeapEmpty(HP* php);
+int HeapSize(HP* php);
